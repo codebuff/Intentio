7c0558cdfee396722e25ea153eb86c526c922094
deepankar <deepankar@codebuff.com>
2014-11-18 04:44:51 +0530

    change app theme back to APPtheme from appcompat added helper class for preferences now default settings are initialized when app runs for the first time

--- app/src/main/java/net/codebuff/intentio/MainActivity.java
+++ app/src/main/java/net/codebuff/intentio/MainActivity.java
@@ -2,6 +2,7 @@ package net.codebuff.intentio;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,6 +22,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        PreferenceManager.setDefaultValues(this, R.xml.pref_general, false); // saving the default preferences for the first time
         setContentView(R.layout.activity_main);
     }
 
