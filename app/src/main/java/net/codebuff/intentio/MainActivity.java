654cfb947707d05cab65963fba43e2ab16333633
chhavi_sharma <fiorella.chhavi@gmail.com>
2014-12-02 22:52:57 +0530

    Added icon image for multiple devices. placed buttons ad text on Second_demo_screen.

--- app/src/main/java/net/codebuff/intentio/MainActivity.java
+++ app/src/main/java/net/codebuff/intentio/MainActivity.java
@@ -1,84 +1,58 @@
 package net.codebuff.intentio;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import net.codebuff.intentio.parser.Parser;
-import net.codebuff.intentio.preferences.PrefsManager;
 import net.codebuff.intentio.preferences.SettingsActivity;
 
 import java.io.IOException;
-import java.util.Calendar;
 
 
 public class MainActivity extends ActionBarActivity {
     private Button parse_button ;
     private TextView xl_data;
     Context context;
-    PrefsManager app ;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         PreferenceManager.setDefaultValues(this, R.xml.pref_general, false); // saving the default preferences for the first time
-        context = getApplicationContext();
-        app = new PrefsManager(context);
-        if(app.first_run()){
-            // open the parser activity
-            if(savedInstanceState == null) {
-                startActivity(new Intent(this , Parser_demo.class));
-                finish();
-            }
-        }else{
-            if(savedInstanceState == null) {
-                startActivity(new Intent(this , demo_2.class));
-                finish();
-            }
-        }
         setContentView(R.layout.activity_main);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        context = getApplicationContext();
-        app = new PrefsManager(context);
-        if(app.first_run()){
-            // open the parser activity
-            startActivity(new Intent(this , Parser_demo.class));
-            finish();
-        }else{
-            startActivity(new Intent(this , demo_2.class));
-            finish();
-        }
-    }
+        context = this.getApplicationContext();
+       final Parser parser = new Parser();
+        parse_button = (Button)findViewById(R.id.parse_button);
+        xl_data = (TextView)findViewById(R.id.data);
+        parse_button.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+               // Intent intent = new Intent(context,IntentioService.class);
+               // intent.putExtra("method","method_name");
+               // startService(intent);
+                //NotificationCentre.notify(context,"notification of intention",0);
+                try {
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        context = getApplicationContext();
-        app = new PrefsManager(context);
-        if(app.first_run()){
-            // open the parser activity
-                startActivity(new Intent(this , Parser_demo.class));
-                finish();
-        }else{
-                startActivity(new Intent(this , demo_2.class));
-                finish();
-        }
+                    xl_data.setText(parser.parse_excel());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 
     @Override
